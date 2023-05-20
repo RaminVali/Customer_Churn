e8465f523324270c130c957a09f35bdb952f6ced
RaminVali <60187816+RaminVali@users.noreply.github.com>
2023-05-20 13:33:51 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Telco Customer Churn Prediction
+
+data originally from [here](https://community.ibm.com/community/user/businessanalytics/blogs/steven-macko/2019/07/11/telco-customer-churn-1113)
+
+Got retrieved from hugging face [here](https://huggingface.co/datasets/scikit-learn/churn-prediction)
+
---
+++ setup.sh
@@ -0,0 +1,25 @@
+
+#! /usr/bin/sh
+
+echo "Welcome to the Customer CHurn Prediction Project:"
+
+# Create workspace
+echo "Create a resource group:"
+az group create --name "rg-churn-pred-proj" --location "eastus"
+
+echo "Create an Azure Machine Learning workspace:"
+az ml workspace create --name "churn-pred-proj" -g "rg-churn-pred-proj"
+
+# Create compute instance
+guid=$(cat /proc/sys/kernel/random/uuid)
+suffix=${guid//[-]/}
+suffix=${suffix:0:18}
+
+ComputeName="ci${suffix}"
+
+echo "Creating a compute instance with name: " $ComputeName
+az ml compute create --name ${ComputeName} --size STANDARD_DS11_V2 --type ComputeInstance -w churn-pred-proj -g rg-churn-pred-proj
+
+# Create compute cluster
+echo "Creating a compute cluster with name: aml-cluster"
+az ml compute create --name "aml-cluster" --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute -w churn-pred-proj -g rg-churn-pred-proj
